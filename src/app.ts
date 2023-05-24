a7a7804cb4565e90f4d913e9273ee021281d5151
pmcg31 <seiffertrk@gmail.com>
2023-05-23 21:18:03 -0500

    Added some message parsing; Now tracks player online status; Mutations are forward to all connected clients for now

--- src/app.ts
+++ src/app.ts
@@ -3,73 +3,47 @@ dotenv.config();
 
 import fs from 'fs';
 import https from 'https';
-import express from 'express';
+import http from 'http';
+import express, { Request, Response } from 'express';
 import expressWs from 'express-ws';
-
-const connections = [];
-
-function addConn(from, ws) {
-  let isKnown = false;
-  connections.forEach((connection) => {
-    if (connection.from === from && connection.ws === ws) {
-      isKnown = true;
-    }
-  });
-  if (!isKnown) {
-    console.log(`new connection from ${from}`);
-    connections.push({ from, ws });
-  } else {
-    console.log(`existing connection from ${from}`);
-  }
-  console.log(`there are now ${connections.length} connections`);
-}
-
-function removeConn(ws) {
-  for (let idx = connections.length - 1; idx >= 0; idx--) {
-    if (connections[idx].ws === ws) {
-      // Remove this item
-      connections.splice(idx, 1);
-    }
-  }
-  console.log(`there are now ${connections.length} connections`);
-}
+import * as ws from 'ws';
+import { acceptConnection } from './conn-manager';
 
 const options = {
-  key: fs.readFileSync(process.env.SSL_KEY_FILE),
-  cert: fs.readFileSync(process.env.SSL_CERT_FILE)
+  key: fs.readFileSync(process.env.SSL_KEY_FILE as fs.PathLike),
+  cert: fs.readFileSync(process.env.SSL_CERT_FILE as fs.PathLike)
 };
-const port = process.env.PORT;
+const port = Number(process.env.LOCAL_PORT);
+const hostname = process.env.LOCAL_HOSTNAME;
+const sslEnable =
+  process.env.SSL_ENABLE && process.env.SSL_ENABLE.toLowerCase() === 'true';
 const exp = express();
-const server = https.createServer(options, exp);
+
+let server = null;
+if (sslEnable) {
+  server = https.createServer(options, exp);
+} else {
+  server = http.createServer(exp);
+}
 
 const { app /*, getWss, applyTo */ } = expressWs(exp, server);
 
-app.get('/', (req, res) => {
-  res.status(200).send('Welcome to our app');
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).send("This is not the page you're looking for");
 });
 
-app.ws('/ws', async function (ws, req) {
-  const from = `${req.socket.remoteAddress}:${req.socket.remotePort}`;
-
-  addConn(from, ws);
-
-  ws.addEventListener('close', () => {
-    console.log(`from: ${from} is outta here!`);
-    removeConn(ws);
-  });
-
-  ws.addEventListener('message', (e) => {
-    const msg = e.data;
-    console.log(`from: ${from} msg: ${JSON.stringify(msg)}`);
-
-    const obj = JSON.parse(msg);
-    connections.forEach((connection) => {
-      if (connection.ws.readyState === ws.OPEN) {
-        connection.ws.send(JSON.stringify({ data: obj }));
-      }
-    });
-  });
+app.ws('/ws', async function (ws: ws.WebSocket, req: Request) {
+  acceptConnection(
+    req.socket.remoteAddress as string,
+    req.socket.remotePort as number,
+    ws
+  );
 });
 
-server.listen(port);
-console.log(`started server, url: https://localhost:${port}`);
+if (sslEnable) {
+  server.listen(port, hostname);
+  console.log(`started server, url: https://${hostname}:${port}`);
+} else {
+  server.listen(port, hostname);
+  console.log(`started server, url: http://${hostname}:${port}`);
+}
---
+++ src/conn-manager.ts
@@ -0,0 +1,103 @@
+import * as ws from 'ws';
+
+type ConnMapValue = {
+  remoteAddress: string;
+  remotePort: number;
+  playerId?: string;
+};
+
+type QueryMapValue = {
+  ws: ws.WebSocket;
+  key: object;
+};
+
+const connectionMap = new Map<ws.WebSocket, ConnMapValue>();
+
+function handleMessage(e: ws.MessageEvent) {
+  const conn = connectionMap.get(e.target);
+  if (conn) {
+    const data = JSON.parse(e.data as string);
+    if (Object.prototype.hasOwnProperty.call(data, 'playerId')) {
+      if (conn.playerId) {
+        console.log(
+          `${conn.remoteAddress}[${conn.remotePort}] Player offline: ${conn.playerId}`
+        );
+      }
+      conn.playerId = data.playerId;
+      if (conn.playerId) {
+        console.log(
+          `${conn.remoteAddress}[${conn.remotePort}] Player online: ${data.playerId}`
+        );
+      }
+    } else if (Object.prototype.hasOwnProperty.call(data, 'usingQuery')) {
+      console.log(
+        `${conn.remoteAddress}[${conn.remotePort}] usingQuery: ${JSON.stringify(
+          data.usingQuery
+        )}`
+      );
+    } else if (Object.prototype.hasOwnProperty.call(data, 'releaseQuery')) {
+      console.log(
+        `${conn.remoteAddress}[${
+          conn.remotePort
+        }] releaseQuery: ${JSON.stringify(data.releaseQuery)}`
+      );
+    } else if (Object.prototype.hasOwnProperty.call(data, 'mutation')) {
+      console.log(
+        `${conn.remoteAddress}[${conn.remotePort}] mutation: ${
+          Object.keys(data.mutation)[0]
+        }`
+      );
+      const msg = JSON.stringify({ mutation: Object.keys(data.mutation)[0] });
+      for (const ws of connectionMap.keys()) {
+        const tmp = connectionMap.get(ws);
+        if (tmp) {
+          console.log(`${tmp.remoteAddress}[${tmp.remotePort}] ===> ${msg}`);
+        }
+        ws.send(msg);
+      }
+    }
+  }
+}
+
+function handleClose(e: ws.CloseEvent) {
+  const conn = connectionMap.get(e.target);
+  if (conn) {
+    if (conn.playerId) {
+      console.log(
+        `${conn.remoteAddress}[${conn.remotePort}] Player offline: ${conn.playerId}`
+      );
+    }
+
+    // Remove all query map values using this
+    // conection
+
+    // Remove from connection map
+    connectionMap.delete(e.target);
+
+    console.log(
+      `${conn.remoteAddress}[${conn.remotePort}] is outta here! total connections: ${connectionMap.size}`
+    );
+  }
+}
+
+export function acceptConnection(
+  remoteAddress: string,
+  remotePort: number,
+  ws: ws.WebSocket
+) {
+  // Save the connection
+  connectionMap.set(ws, {
+    remoteAddress,
+    remotePort
+  });
+
+  // Handle messages from this connection
+  ws.addEventListener('message', handleMessage);
+
+  // Handle the closing of this connection
+  ws.addEventListener('close', handleClose);
+
+  console.log(
+    `Accepted connection from ${remoteAddress}[${remotePort}]; total connections: ${connectionMap.size}`
+  );
+}
